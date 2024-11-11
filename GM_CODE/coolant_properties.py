cc8db8b4a4e8bf8f40aeaf9914519de45a95b7f4
giorguy <gm1422@ic.ac.uk>
2024-11-11 14:09:39 +0000

    updates to code

--- GM_CODE/coolant_properties.py
+++ GM_CODE/coolant_properties.py
@@ -3,7 +3,7 @@ import CoolProp as CP
 from pyfluids import Fluid, Mixture, FluidsList, Input
 
 ethanol = Fluid(FluidsList.Ethanol).with_state(
-    Input.pressure(4e6), Input.temperature(75) #at 4 MPa and 350 K
+    Input.pressure(4.5e6), Input.temperature(75) #at 4.5 MPa and 335 K (will enter nozzle at 300 K, assume there will be 35 K increase on temp up to throat)
 )
 
 rho = ethanol.density 
@@ -11,14 +11,17 @@ conductivity = ethanol.conductivity
 mu = ethanol.dynamic_viscosity
 C_p = ethanol.specific_heat
 
-m_tot = 0.13 #kg/s as estimated by rpa
+m_tot = 0.135 #kg/s as estimated by rpa
 N= 50 # number of channels (assumed)
 m_channel = m_tot/N
-d = 0.002 #assumed
-A = d**2
+h = 0.001 #assumed
+w = 0.0005 #width at throat must be very small as it has small region
+A = h*w
 v = m_channel/(rho*A)
 
-Re = rho*v*d/mu
+d = 1.30*(h*w)**0.625/(h+w)**0.25 #https://www.engineeringtoolbox.com/equivalent-diameter-d_205.html
+print(d)
+Re = rho*v*d/mu #as long as Re>4000 we can use the equation below
 print(Re)
 Pr = mu*C_p/conductivity
 h_coolant = 0.023*conductivity/d*Re**0.8*Pr**0.4
--- GM_CODE/simulteneous equations.py
+++ GM_CODE/simulteneous equations.py
@@ -1,29 +1,28 @@
 import numpy as np
 from coolant_properties import h_coolant
 
-h_g = 10000 #value from Bartz Equation solved by OB code
-t_w = 0.002 #thickn
-k = 125 #thermal conductivity of copper alloy
+h_g = 7737 #value from RPA simulation
+t_w = 0.001 #thickness
+k = 100 #thermal conductivity of steel alloy
 h_l = h_coolant
-T_g = 2200 #temp of gas as found from RPA
+T_g = 2057 #temp of gas as found from RPA
+T_l = 335 #in kelvin, assumed for the throat
 
 A = np.array([
-    [1/h_g+t_w/k+1/h_l, 1, 0, 0],  # First equation coefficients
-    [1/h_g, 0, 1, 0],  # Second equation coefficients
-    [t_w/k, 0, -1, 1],  # Third equation coefficients
-    [1/h_l, 1, 0, -1]   # Fourth equation coefficients
+    [1/h_l,0,-1],  # First equation coefficients
+    [1/h_g, 1, 0],  # Second equation coefficients
+    [t_w/k,-1, 1],  # Third equation coefficients
 ])
 
-B = np.array([T_g, T_g, 0, 0])  # Constant terms
+B = np.array([-T_l, T_g, 0])  # Constant terms
 
 # Solve the equations
 try:
     solution = np.linalg.solve(A, B)
     print("Solution:")
-    print("q=", solution[0])
-    print("T_L =", solution[1])
-    print("T_wg =", solution[2])
-    print("T_wl =", solution[3])
+    print("q=", solution[0]*10**-3, 'kW/(m^2*K)')
+    print("T_wg =", solution[1], 'K')
+    print("T_wl =", solution[2], 'K')
 
 except np.linalg.LinAlgError:
     print("The system of equations has no unique solution.")
